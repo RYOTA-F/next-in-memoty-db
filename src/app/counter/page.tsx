ffad0aa53c1bc7553da42ef949979220c3ee7759
RYOTA-F <ry.fj0304@gmail.com>
2024-02-29 12:40:59 +0900

    :sparkles: CounterPage

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useStore } from '@nanostores/react'
+import { $counter } from '@/app/stores/counter/counter.state'
+
+export default function CounterPage() {
+  const count = useStore($counter)
+
+  const addCount = () => {
+    $counter.set(count + 1)
+  }
+
+  return (
+    <main className="flex items-center justify-center p-40">
+      <div>
+        <h1>{count}</h1>
+
+        <button onClick={addCount}>+</button>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main className="flex items-center justify-center p-40">
-      <h1>Test</h1>
+      <Link href="/counter">Counter</Link>
     </main>
-  );
+  )
 }
