c699494b3708974aeb388fa3630aaffae17b4a7e
RYOTA-F <ry.fj0304@gmail.com>
2024-03-10 12:42:30 +0900

    :sparkles: db.current

--- src/db/datascript/todo.ts
+++ src/db/datascript/todo.ts
@@ -1,7 +1,8 @@
 import ds from 'datascript'
+import { v4 as uuid } from 'uuid'
 
 const transact = ds.db_with
-let db = ds.empty_db()
+const db = { current: ds.empty_db() }
 
 interface AddTodoAction {
   text: string
@@ -11,25 +12,28 @@ interface AddTodoAction {
  * Todoを追加
  */
 export const addTodo = (action: AddTodoAction) => {
+  const id = uuid()
   const transaction = [
     {
       ':db/id': -1,
+      uniqueId: id,
       text: action.text,
       completed: false,
     },
   ]
 
-  db = transact(db, transaction)
-  return db
+  db.current = transact(db.current, transaction)
+  return db.current
 }
 
 /**
  * Todo一覧を取得
  */
 export const getTodos = () => {
-  const query = `[:find ?e ?text ?completed
-    :where [?e "text" ?text]
+  const query = `[:find ?e ?uniqueId ?text ?completed
+    :where [?e "uniqueId" ?uniqueId]
+           [?e "text" ?text]
            [?e "completed" ?completed]]`
 
-  return ds.q(query, db)
+  return ds.q(query, db.current)
 }
